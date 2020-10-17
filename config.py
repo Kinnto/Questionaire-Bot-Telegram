9b61b23c3bdba05398d5af05a049a80f22be3cbf
XinTong Li <kicw118@gmail.com>
2020-10-17 00:21:53 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding:utf-8
+
+# configuration
+import os
+
+# Mandatory
+TOKEN = "your token"
+# Optional, if you leave TURING_KEY with blank, the robot won't chat with you.
+TURING_KEY = 'KEY'
+
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bot.db')
+# minutes
+INTERVAL = 120
+
+LOGGER = False
